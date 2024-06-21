44813d91af17774d4ad879b0dd869bed8778a3f9
EmmanuelCastaneda <151039652+EmmanuelCastaneda@users.noreply.github.com>
2024-06-21 08:02:01 -0500

    settitgs 1

--- Backend/proyecto_senauthenticator/settings.py
+++ Backend/proyecto_senauthenticator/settings.py
@@ -138,3 +138,11 @@ CORS_ALLOWED_ORIGINS = ['http://localhost:3000']
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.coreapi.AutoSchema",
 }
+
+
+
+
+
+
+
+#'rest_framework.authtoken',
