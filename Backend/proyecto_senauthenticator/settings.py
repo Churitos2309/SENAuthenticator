eeae5763139665da57fa80fa730a9b91899a6690
EmmanuelCastaneda <151039652+EmmanuelCastaneda@users.noreply.github.com>
2024-06-21 08:11:27 -0500

    settings

--- Backend/proyecto_senauthenticator/settings.py
+++ Backend/proyecto_senauthenticator/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -112,9 +113,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-co'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Bogota'
 
 USE_I18N = True
 
@@ -124,14 +125,22 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# autoriza rutas para poderse ejecutar (en este caso la de Next.js)
+# Permite todas las solicitudes de todos los dominios
+CORS_ALLOW_ALL_ORIGINS = True
+
+# autoriza rutas para poderse ejecutar (en este caso la de Next.js y la de Flutter)
 CORS_ALLOWED_ORIGINS = ['http://localhost:3000']
 
 # Configuración de esquema para que django rest framework y coreapi puedan documentar el código
